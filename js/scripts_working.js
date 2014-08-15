1a248e217533aec5776de87b01116baa9997a338
lrdeyo <laura@ldeyomac.local>
2014-08-14 18:48:05 -0700

    Add verification on entry & remove added folder

--- js/scripts_working.js
+++ js/scripts_working.js
@@ -36,8 +36,6 @@ $( function() {
 	// Verify entry is a number & if board is completed correctly
 	// **********	
 	$( 'input[type=number]' ).keypress( function( evt ) {
-		// Take care of styling in case previous entry was invalid
-		$(this).removeClass( 'invalid' );
 		// If entry is not a number do not allow to be entered
 		// Else if entry is a number but a value exist clear that value to allow to be overwritten
 		if ( !String.fromCharCode( evt.keyCode ).match( /[1-9\.]/ ) ) {
@@ -48,53 +46,17 @@ $( function() {
 		// Verify the full board is correct
 		// Need to use input/propertychange allow to be able to get the most recently entered input
 		$(this).on( 'input propertychange', function() {
-			var currentValidate = false,
-				entryVal = $(this).val(),
-				rowClass = $(this).attr("class").match(/row[\w-]*\b/),
-				columnClass = $(this).attr("class").match(/column[\w-]*\b/),
-				boxClass = $(this).attr("class").match(/box[\w-]*\b/);
-
-			// Validate the row, the column and the box of the entry
-			if( validateInput( rowClass[0], entryVal ) ) {
-				if( validateInput( columnClass[0], entryVal ) ) {
-					if( validateInput( boxClass[0], entryVal ) ) {
-						currentValidate = true;
-					}
-				}
-			}
-			// If the entry did not validate within its own row, column, and box signal to user
-			// Else validate all rows, then all columns, then all 9 input 'boxes'
-			if( !currentValidate ) {
-				$(this).addClass( 'invalid' );
-			} else {
-				if( validateSection( '.row' ) ) {
-					if( validateSection( '.column' ) ) {
-						if( validateSection( '.box' ) ) {
-							$( '.win' ).show();
-						}
+			// Validate all rows, then all columns, then all 9 input 'boxes'
+			if( validateSection( '.row' ) ) {
+				if( validateSection( '.column' ) ) {
+					if( validateSection( '.box' ) ) {
+						$( '.win' ).show();
 					}
 				}
 			}
 		});
 	});
-	// Helper function for validating the row, column and box the input is in
-	var validateInput = function ( section, entryVal ) {
-		var isDup = false;
-		$( '.' + section ).each( function() {
-			// Need to not check the input currently being entered
-			if( !$(this).is( ':focus' ) ) {
-				if( $(this).val() === entryVal ) {
-					isDup = true;
-				}
-			}
-		});
-		if ( isDup ) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	// Helper function for validating full puzzle
+	// Helper function for validating rows, columns and boxes
 	var validateSection = function( section ) {
 		// 9 of each: rows, columns and boxes
 		for( var secCounter = 1; secCounter <= 9; secCounter++ ) {
